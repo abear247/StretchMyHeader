78de82819866a73a6d8dbe0b7a70469eeae0a8de
alexbearinger <alexbearinger@Alexs-MacBook-Pro.local>
2017-02-14 10:51:46 -0500

    first commit

---
+++ StretchMyHeader/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  StretchMyHeader
+//
+//  Created by Alex Bearinger on 2017-02-14.
+//  Copyright © 2017 Alex Bearinger. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
